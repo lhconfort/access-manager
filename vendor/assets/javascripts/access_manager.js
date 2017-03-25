033f46a7bbe628b1c6ae2e62ee57473352d0a9bc
unformatt <unformatt@gmail.com>
2017-03-25 08:52:54 -0300

    removed submenu, fix on javascript

--- vendor/assets/javascripts/access_manager.js
+++ vendor/assets/javascripts/access_manager.js
@@ -1,27 +1,25 @@
-//     Access Manager 0.1.0
-//     (c) 2014
-//     Access Manager may be freely distributed under the MIT license.
-
 $(function () {
-  var userActions = jQuery.parseJSON($('#current_user_actions').val());
+  if ($('#current_user_actions').size() == 1) {
+    var userActions = jQuery.parseJSON($('#current_user_actions').val());
 
-  _.each($('[data-required-action]'), function (val) {
-    var requiredActions = $(val).data('required-action').split(',');
+    _.each($('[data-required-action]'), function (val) {
+      var requiredActions = $(val).data('required-action').split(',');
 
-    if (_.intersection(userActions, requiredActions).length == 0) {
-      $(val).remove();
-    }
-  });
+      if (_.intersection(userActions, requiredActions).length == 0) {
+        $(val).remove();
+      }
+    });
 
-  _.each($('[data-menu=submenu]'), function (val) {
-    if ($(val).find('[data-required-action]').length == 0) {
-      $(val).remove();
-    }
-  });
+    _.each($('[data-menu=submenu]'), function (val) {
+      if ($(val).find('[data-required-action]').length == 0) {
+        $(val).remove();
+      }
+    });
 
-  _.each($('[data-menu=menu]'), function (val) {
-    if ($(val).find('[data-required-action]').length == 0) {
-      $(val).remove();
-    }
-  });
+    _.each($('[data-menu=menu]'), function (val) {
+      if ($(val).find('[data-required-action]').length == 0) {
+        $(val).remove();
+      }
+    });
+  }
 });
